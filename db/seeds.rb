9e0ba6ed4983124e87a89d1a9460f75d06110972
Josey Sekine <ipho.7johero@icloud.com>
2022-11-20 20:24:38 -0600

    design add bootstrap

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# accounts
+
+
+20.times do |n|
+  select_name = Faker::Sports::Football.player
+  address = "#{Faker::Sports::Football.team.split(" ")[0]}@com"
+
+  while User.exists?(name: select_name)
+    select_name = Faker::Sports::Football.player
+  end
+
+  while User.exists?(email: address)
+    address = "#{Faker::Sports::Football.team.split(" ")[0]}@com"
+  end
+
+  user_now = User.new(
+    name: select_name,
+    email: address,
+    password: "111111"
+  )
+
+  user_now.save
+
+  3.times do |t|
+    Post.create(
+      content: Faker::Quote.famous_last_words,
+      user_id: user_now.id
+    )
+  end
+end
