f4df4eaed7da6a50cce2105cc96a0a902df0454c
Josey Sekine <ipho.7johero@icloud.com>
2022-11-25 11:27:40 -0600

    db update

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2022_11_22_055502) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -48,7 +51,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_22_055502) do
 
   create_table "posts", force: :cascade do |t|
     t.string "content"
-    t.integer "user_id"
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
